044cd938461e600ad1cc6802d426c2ad19dd8117
Daniel Wright <daniel.wright1207@gmail.com>
2021-01-22 14:47:45 -0500

    will repeat after lunch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
+export const FETCH_SMURF_START = "FETCH_SMURF_START";
+export const FETCH_SMURF_SUCCESS = "FETCH_SMURF_SUCCESS";
+export const FETCH_SMURF_FAIL = "FETCH_SMURF_FAIL";
+export const ADD_SMURF = "ADD_SMURF";
 //Task List:
-//1. Add fetch smurfs action: 
+//1. Add fetch smurfs action:
 //              - fetch and return initial list of smurfs
 //              - dispatch actions that indicate if we are waiting for a server response
 //              - dispatch an error text action if an error is returned from the server
+export const getSmurfs = () => (dispatch) => {
+  dispatch({ type: FETCH_SMURF_START });
+  axios
+    .get("http://localhost:3333/smurfs")
+    .then((res) => {
+      console.log(res.data);
+      dispatch({ type: FETCH_SMURF_SUCCESS, payload: res.data });
+    })
+    .catch((err) => {
+      dispatch({ type: FETCH_SMURF_FAIL, payload: err.response.code });
+    });
+};
+
+export const newSmurf = () => (dispatch) => {
+    dispatch({ type: ADD_SMURF });
+    axios.post("http://localhost:3333/smurfs")
+        .then((res) => {
+            console.log(res.data);
+            dispatch({type: })
+        });
+}
+
 //2. Add add smurf action:
 //              - dispatch an error text action if smurf data does not includes a name, nickname and position field
 //              - send a post request with the smurf as body to see if there is an error
